2e80990509d62f4e79cc93d799dc75f19af4f05b
Mayank <mayank290404gupta@gmail.com>
2024-09-20 01:08:08 +0530

    Styling updated

--- popup/popup.js
+++ popup/popup.js
@@ -1,40 +1,73 @@
-const addElement = (className,innerText,parent) => {
-  const div = document.createElement('div')
-  div.className = className
-  div.innerText = innerText
-  parent.appendChild(div)
+const addElement = (className ="",innerText="",parent="",type="div") => {
+  const div = document.createElement(type)
+  if(className!=="") div.className = className
+  if(innerText!=="") div.innerText = innerText
+  if(parent!=="") parent.appendChild(div)
   return div
 }
+
+const styleUpdate = (result,div) => {
+  if(result.toUpperCase()==="ACCEPTED"){
+    div.style.color="green"
+  }
+  else if(result.toUpperCase()==="WRONG"){
+    div.style.color="red"
+  }
+  else{
+    div.style.color="yellow"
+  }
+}
+
+const updateUI = () => {
+  const qs = document.querySelector(".others");
+  qs.innerText = "No Recent Submissions"; 
+  qs.className="noRecSub"
+};
+
+const memoryCleaner = (btn) => {
+  btn.addEventListener("click",(e)=>{
+    chrome.storage.local.clear()
+    updateUI()
+  })
+}
+
 
 chrome.tabs.query({ active: true }, (tabs) => {
   const url = tabs[0].url;
   if (url.includes("codechef.com")) {
     let qs = document.querySelector(".others");
+    qs.className=""
+
     chrome.storage.local.get(null, (res) => {
-      if(res.length===0) {
-        qs.innerText="No Recent Submissions"
+      let keys = Object.keys(res)
+      let len = keys.length
+
+      if(len===0) {
+        addElement("noRecSub","No Recent Submissions",qs)
         return
       }
-      const subHeading = addElement("subHeading","Recent Submissions",qs) 
-      qs.appendChild(subHeading)
-      let count = 5;
-      for (let val in res) {
-        const div = document.createElement('div')
-        const divChild1 = document.createElement('div');
-        const divChild2 = document.createElement('div');
-        divChild1.innerText = res[val].name
-        divChild2.innerText = res[val].result
-        div.appendChild(divChild1)
-        div.appendChild(divChild2)
-        div.className = "qList"
-        qs.appendChild(div)
-        if(!count--) break;
+
+      const subHeading = addElement("subHeading","",qs)
+      addElement("","Recent Submissions",subHeading) 
+      memoryCleaner(addElement("btn","Clear",subHeading,"button"))
+
+      let count = Math.min(5,len);
+      for (let i=count-1;i>=0;i--) {
+        const div = addElement("qList","",qs)
+        const divChild1 = addElement("qName",res[keys[i]].name,div)
+        const divChild2 = addElement("qresult",res[keys[i]].result.toUpperCase(),div)
+        styleUpdate(res[keys[i]].result,divChild2)
       }
+
+      // clearing other submissions
+      for(let i=count;i<len;i++){
+        chrome.storage.local.remove(keys[i]);
+      } 
     });
   } 
   else {
-    let qs = document.querySelector(".others");
-    qs.innerText = "Not a CodeChef page";
+    let qs = document.querySelector(".others")
+    addElement("otherPage","Not a CodeChef page",qs)
   }
 
 });
